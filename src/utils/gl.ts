e77a91274b497cbc46ac4b4e4cfc57c5dfbb235b
alphardex <2582347430@qq.com>
2022-03-14 09:40:45 +0800

    add iterateBuffer function

--- src/utils/gl.ts
+++ src/utils/gl.ts
@@ -6,4 +6,21 @@ const makeBuffer = (count = 100, fn: any, dimension = 3) => {
   return buffer;
 };
 
-export { makeBuffer };
+// 迭代buffer
+const iterateBuffer = (
+  buffer: ArrayLike<number>,
+  count: number,
+  fn: any,
+  dimension = 3
+) => {
+  for (let i = 0; i < count; i++) {
+    const axis = i * dimension;
+    const x = axis;
+    const y = axis + 1;
+    const z = axis + 2;
+    const w = axis + 3;
+    fn(buffer, { x, y, z, w });
+  }
+};
+
+export { makeBuffer, iterateBuffer };
