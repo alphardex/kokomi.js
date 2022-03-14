d6599c36c80efb56c6eb743a9bfe51727697f458
alphardex <2582347430@qq.com>
2022-03-14 17:13:37 +0800

    add function enableRealisticRender

---
+++ src/utils/misc.ts
@@ -0,0 +1,13 @@
+import * as THREE from "three";
+
+// 开启真实渲染
+const enableRealisticRender = (renderer: THREE.WebGLRenderer) => {
+  renderer.physicallyCorrectLights = true;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.toneMapping = THREE.ReinhardToneMapping;
+  renderer.toneMappingExposure = 3;
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+};
+
+export default { enableRealisticRender };
