8e877fe9ed4b613d226703007770fbb357557910
alphardex <2582347430@qq.com>
2022-08-09 17:15:16 +0800

    v1.9.35

--- src/components/iMouse.ts
+++ src/components/iMouse.ts
@@ -6,6 +6,10 @@ import { Base } from "../base/base";
 class IMouse extends Component {
   mouse: THREE.Vector2;
   mouseDOM: THREE.Vector2;
+  mouseScreen: THREE.Vector2;
+  prevMouseDOM: THREE.Vector2;
+  isMouseMoving: boolean;
+  mouseMoveOffset: number;
   constructor(base: Base) {
     super(base);
 
@@ -14,44 +18,96 @@ class IMouse extends Component {
 
     const mouseDOM = new THREE.Vector2(0, 0);
     this.mouseDOM = mouseDOM;
+
+    const mouseScreen = new THREE.Vector2(0, 0);
+    this.mouseScreen = mouseScreen;
+
+    this.prevMouseDOM = new THREE.Vector2(0, 0);
+    this.isMouseMoving = false;
+    this.mouseMoveOffset = 4;
+  }
+  getMouse(x: number, y: number) {
+    const mouse = new THREE.Vector2(x, window.innerHeight - y);
+    return mouse;
+  }
+  getMouseDOM(x: number, y: number) {
+    const mouseDOM = new THREE.Vector2(x, y);
+    return mouseDOM;
+  }
+  getMouseScreen(x: number, y: number) {
+    const mouseScreen = new THREE.Vector2(
+      x - window.innerWidth / 2,
+      -(y - window.innerHeight / 2)
+    );
+    return mouseScreen;
   }
   listenForMouse() {
     window.addEventListener("mousemove", (e) => {
-      const iMouseNew = new THREE.Vector2(
-        e.clientX,
-        window.innerHeight - e.clientY
-      );
+      const iMouseNew = this.getMouse(e.clientX, e.clientY);
       this.mouse = iMouseNew;
 
-      const mouseDOM = new THREE.Vector2(e.clientX, e.clientY);
+      const mouseDOM = this.getMouseDOM(e.clientX, e.clientY);
       this.mouseDOM = mouseDOM;
+
+      const mouseScreen = this.getMouseScreen(e.clientX, e.clientY);
+      this.mouseScreen = mouseScreen;
     });
     window.addEventListener("touchstart", (e) => {
-      const iMouseNew = new THREE.Vector2(
+      const iMouseNew = this.getMouse(
         e.touches[0].clientX,
-        window.innerHeight - e.touches[0].clientY
+        e.touches[0].clientY
       );
       this.mouse = iMouseNew;
 
-      const mouseDOM = new THREE.Vector2(
+      const mouseDOM = this.getMouse(
         e.touches[0].clientX,
         e.touches[0].clientY
       );
       this.mouseDOM = mouseDOM;
+
+      const mouseScreen = this.getMouseScreen(
+        e.touches[0].clientX,
+        e.touches[0].clientY
+      );
+      this.mouseScreen = mouseScreen;
     });
     window.addEventListener("touchmove", (e) => {
-      const iMouseNew = new THREE.Vector2(
+      const iMouseNew = this.getMouse(
         e.touches[0].clientX,
-        window.innerHeight - e.touches[0].clientY
+        e.touches[0].clientY
       );
       this.mouse = iMouseNew;
 
-      const mouseDOM = new THREE.Vector2(
+      const mouseDOM = this.getMouse(
         e.touches[0].clientX,
         e.touches[0].clientY
       );
       this.mouseDOM = mouseDOM;
+
+      const mouseScreen = this.getMouseScreen(
+        e.touches[0].clientX,
+        e.touches[0].clientY
+      );
+      this.mouseScreen = mouseScreen;
     });
+  }
+  syncMouseDOM() {
+    this.prevMouseDOM.x = this.mouseDOM.x;
+    this.prevMouseDOM.y = this.mouseDOM.y;
+  }
+  judgeIsMouseMoving() {
+    if (
+      Math.abs(this.mouseDOM.x - this.prevMouseDOM.x) < this.mouseMoveOffset &&
+      Math.abs(this.mouseDOM.y - this.prevMouseDOM.y) < this.mouseMoveOffset
+    ) {
+      this.isMouseMoving = false;
+    } else {
+      this.isMouseMoving = true;
+    }
+  }
+  update(time: number): void {
+    this.judgeIsMouseMoving();
+    this.syncMouseDOM();
   }
 }
 
--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -28,4 +28,22 @@ const getEnvmapFromHDRTexture = (
   return envmap;
 };
 
-export { optimizeModelRender, enableRealisticRender, getEnvmapFromHDRTexture };
+// 获取重心坐标系
+const getBaryCoord = (bufferGeometry: THREE.BufferGeometry) => {
+  // https://gist.github.com/mattdesl/e399418558b2b52b58f5edeafea3c16c
+  const length = bufferGeometry.attributes.position.array.length;
+  const count = length / 3;
+  const bary = [];
+  for (let i = 0; i < count; i++) {
+    bary.push(0, 0, 1, 0, 1, 0, 1, 0, 0);
+  }
+  const aCenter = new Float32Array(bary);
+  bufferGeometry.setAttribute("aCenter", new THREE.BufferAttribute(aCenter, 3));
+};
+
+export {
+  optimizeModelRender,
+  enableRealisticRender,
+  getEnvmapFromHDRTexture,
+  getBaryCoord,
+};
