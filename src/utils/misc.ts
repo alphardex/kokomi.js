29d7d7c5697eb892abeb554e9f969ad904b55ae5
alphardex <2582347430@qq.com>
2022-08-09 15:01:02 +0800

    add 2 utils

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -1,5 +1,11 @@
 import * as THREE from "three";
 
+// 优化模型渲染
+const optimizeModelRender = (renderer: THREE.WebGLRenderer) => {
+  renderer.physicallyCorrectLights = true;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+};
+
 // 开启真实渲染
 const enableRealisticRender = (renderer: THREE.WebGLRenderer) => {
   renderer.physicallyCorrectLights = true;
@@ -10,4 +16,16 @@ const enableRealisticRender = (renderer: THREE.WebGLRenderer) => {
   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 };
 
-export { enableRealisticRender };
+// 从hdr贴图中提取envmap
+const getEnvmapFromHDRTexture = (
+  renderer: THREE.WebGLRenderer,
+  texture: THREE.Texture
+) => {
+  const pmremGenerator = new THREE.PMREMGenerator(renderer);
+  pmremGenerator.compileEquirectangularShader();
+  const envmap = pmremGenerator.fromEquirectangular(texture).texture;
+  pmremGenerator.dispose();
+  return envmap;
+};
+
+export { optimizeModelRender, enableRealisticRender, getEnvmapFromHDRTexture };
