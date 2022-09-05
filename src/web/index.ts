b9953585bbe5bd29625b6c4efe6343adc28820d3
alphardex <2582347430@qq.com>
2022-09-05 09:39:49 +0800

    add WheelScroller

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,4 +1,5 @@
 export * from "./gallery";
 export * from "./html";
+export * from "./scroller";
 export * from "./shadertoy";
 export * from "./utils";
---
+++ src/web/scroller.ts
@@ -0,0 +1,45 @@
+import * as THREE from "three";
+
+export interface Scroll {
+  current: number;
+  target: number;
+  ease: number;
+  last: number;
+  delta: number;
+  direction: "up" | "down" | "";
+}
+
+class WheelScroller {
+  scroll: Scroll;
+  constructor() {
+    this.scroll = {
+      current: 0,
+      target: 0,
+      ease: 0.05,
+      last: 0,
+      delta: 0,
+      direction: "",
+    };
+  }
+  // 监听滚动
+  listenForScroll() {
+    window.addEventListener("wheel", (e) => {
+      const newScrollY = e.deltaY;
+      const scrollYDelta = newScrollY;
+      this.scroll.target += scrollYDelta;
+    });
+  }
+  // 同步滚动的数据
+  syncScroll() {
+    this.scroll.current = THREE.MathUtils.lerp(
+      this.scroll.current,
+      this.scroll.target,
+      this.scroll.ease
+    );
+    this.scroll.delta = this.scroll.current - this.scroll.last;
+    this.scroll.direction = this.scroll.delta > 0 ? "down" : "up";
+    this.scroll.last = this.scroll.current;
+  }
+}
+
+export { WheelScroller };
