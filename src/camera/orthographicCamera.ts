f779f8884bd762fa280b16c34b38e3fdfc59c2f1
alphardex <2582347430@qq.com>
2023-06-13 09:35:28 +0800

    make OrthographicCamera aspect optional

--- src/camera/orthographicCamera.ts
+++ src/camera/orthographicCamera.ts
@@ -4,6 +4,7 @@ export interface OrthographicCameraConfig {
   frustum: number;
   near: number;
   far: number;
+  useAspect: boolean;
 }
 
 /**
@@ -14,11 +15,13 @@ class OrthographicCamera extends THREE.OrthographicCamera {
   constructor(config: Partial<OrthographicCameraConfig> = {}) {
     const aspect = window.innerWidth / window.innerHeight;
 
-    const { frustum = 5.7, near = 0.1, far = 2000 } = config;
+    const { frustum = 5.7, near = 0.1, far = 2000, useAspect = true } = config;
+
+    const actualAspect = useAspect ? aspect : 1;
 
     super(
-      aspect * frustum * -0.5,
-      aspect * frustum * 0.5,
+      actualAspect * frustum * -0.5,
+      actualAspect * frustum * 0.5,
       frustum * 0.5,
       frustum * -0.5,
       near,
