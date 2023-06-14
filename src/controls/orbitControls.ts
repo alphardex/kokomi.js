dfd1fd95d7568c267410656fc93011349e1d2244
alphardex <2582347430@qq.com>
2023-06-14 14:07:06 +0800

    component OrbitControls add config

--- src/controls/orbitControls.ts
+++ src/controls/orbitControls.ts
@@ -3,20 +3,26 @@ import { Component } from "../components/component";
 
 import { OrbitControls as OrbitControlsImpl } from "three-stdlib";
 
+export interface OrbitControlsConfig {
+  enableDamping: boolean;
+}
+
 /**
  * A drop-in orbitControls
  */
 class OrbitControls extends Component {
   controls: OrbitControlsImpl;
-  constructor(base: Base) {
+  constructor(base: Base, config: Partial<OrbitControlsConfig> = {}) {
     super(base);
+
+    const { enableDamping = true } = config;
 
     const controls = new OrbitControlsImpl(
       base.camera,
       base.renderer.domElement
     );
     this.controls = controls;
-    controls.enableDamping = true;
+    controls.enableDamping = enableDamping;
   }
   update(time: number): void {
     this.controls.update();
