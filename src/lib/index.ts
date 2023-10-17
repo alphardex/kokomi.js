d4c2d47616a77e0b45d37e66d61e3cd704b4247a
alphardex <2582347430@qq.com>
2023-10-17 13:30:29 +0800

    export extendMaterial

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,5 +2,6 @@ export * from "./Bender";
 export * from "./custom-mipmap-generation";
 export * from "./other";
 export * from "./THREE-CustomShaderMaterial";
+export * from "./THREE-extendMaterial";
 export * from "./three-stdlib";
 export * from "./threejs-thin-film-iridescence";
