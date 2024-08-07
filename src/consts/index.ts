e0cc7afca1708db32487ff9bfc574d7f5775c23c
alphardex <2582347430@qq.com>
2024-08-07 16:32:40 +0800

    update draco decoder url

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,4 +1,4 @@
 const DEFAULT_DRACO_DECODER_PATH =
-  "https://www.gstatic.com/draco/versioned/decoders/1.4.3/";
+  "https://www.gstatic.com/draco/versioned/decoders/1.5.7/";
 
 export { DEFAULT_DRACO_DECODER_PATH };
